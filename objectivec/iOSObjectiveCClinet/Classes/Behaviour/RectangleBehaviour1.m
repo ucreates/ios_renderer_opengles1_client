abb9a48e58abaa2e066218b85154a698529b3efb
ucreates <ucreates.inc@gmail.com>
2020-03-11 00:09:16 +0900

    add rectangle1 demo

---
+++ objectivec/iOSObjectiveCClinet/Classes/Behaviour/RectangleBehaviour1.m
@@ -0,0 +1,36 @@
+// ======================================================================
+// Project Name    : ios_renderer_opengles1_client
+//
+// Copyright © 2020 U-CREATES. All rights reserved.
+//
+// This source code is the property of U-CREATES.
+// If such findings are accepted at any time.
+// We hope the tips and helpful in developing.
+// ======================================================================
+#import "RectangleBehaviour1.h"
+@interface RectangleBehaviour1 ()
+@property double rotate;
+@end
+@implementation RectangleBehaviour1
+@synthesize asset;
+- (id)init {
+    self = [super init];
+    self->asset = [[RectangleAsset1 alloc] init:1.0f height:1.0f color:GLESColor.white];
+    [self->asset create];
+    return self;
+}
+- (void)onCreate:(Parameter*)parameter {
+    return;
+}
+- (void)onDestroy {
+    [self->asset.vertex releaseBuffer];
+    return;
+}
+- (void)onUpdate:(NSTimeInterval)delta {
+    [self->asset.transform setPosition:0.0f y:0.0f z:0.0f];
+    [self->asset.transform setScale:1.0f y:1.0f z:1.0f];
+    [self->asset.transform setRotation:0.0f y:0.0f z:0.0f];
+    [self->asset.vertex setRandomColor];
+    return;
+}
+@end
