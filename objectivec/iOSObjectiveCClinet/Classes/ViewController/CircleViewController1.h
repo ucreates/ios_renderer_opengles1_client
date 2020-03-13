f78cd0388c9bf87c40364b123b0ac364ca961350
ucreates <ucreates.inc@gmail.com>
2020-03-14 01:54:50 +0900

    add circle1 demo

---
+++ objectivec/iOSObjectiveCClinet/Classes/ViewController/CircleViewController1.h
@@ -0,0 +1,16 @@
+// ======================================================================
+// Project Name    : ios_renderer_opengles1_client
+//
+// Copyright © 2020 U-CREATES. All rights reserved.
+//
+// This source code is the property of U-CREATES.
+// If such findings are accepted at any time.
+// We hope the tips and helpful in developing.
+// Circle1
+// ======================================================================
+#import <GLKit/GLKit.h>
+#import <OpenGLES/ES1/gl.h>
+#import <OpenGLES/ES1/glext.h>
+#import "CircleViewController1.h"
+@interface CircleViewController1 : GLKViewController <GLKViewControllerDelegate>
+@end
