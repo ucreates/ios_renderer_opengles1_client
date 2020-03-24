399d3bcddadf85b7e8ea1a3ddc76d28c75108f5f
ucreates <ucreates.inc@gmail.com>
2020-03-25 00:17:34 +0900

    add texture demo

---
+++ objectivec/iOSObjectiveCClinet/Classes/Behaviour/TextureBehaviour2.m
@@ -0,0 +1,37 @@
+// ======================================================================
+// Project Name    : ios_renderer_opengles1_client
+//
+// Copyright © 2020 U-CREATES. All rights reserved.
+//
+// This source code is the property of U-CREATES.
+// If such findings are accepted at any time.
+// We hope the tips and helpful in developing.
+// Triangle1
+// ======================================================================
+#import "TextureBehaviour2.h"
+@interface TextureBehaviour2 ()
+@end
+@implementation TextureBehaviour2
+@synthesize asset;
+- (id)init {
+    self = [super init];
+    NSBundle* bundle = [NSBundle mainBundle];
+    NSString* path = [bundle pathForResource:@"texture02" ofType:@"jpg"];
+    self->asset = [[TriangleAsset1 alloc] init:1.0f height:1.0f color:GLESColor.white];
+    [self->asset create:path];
+    return self;
+}
+- (void)onCreate:(Parameter*)parameter {
+    return;
+}
+- (void)onUpdate:(NSTimeInterval)delta {
+    [self->asset.transform setPosition:0.0f y:0.0f z:0.0f];
+    [self->asset.transform setScale:1.0f y:1.0f z:1.0f];
+    [self->asset.transform setRotation:0.0f y:0.0f z:0.0f];
+    return;
+}
+- (void)onDestroy {
+    [self->asset dispose];
+    return;
+}
+@end
