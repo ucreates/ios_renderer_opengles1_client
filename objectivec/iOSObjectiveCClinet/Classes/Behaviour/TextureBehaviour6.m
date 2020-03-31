d394e519455e67c4580eca0daa2ce22829a81992
ucreates <ucreates.inc@gmail.com>
2020-04-01 00:12:15 +0900

    add circle1 texture demo

---
+++ objectivec/iOSObjectiveCClinet/Classes/Behaviour/TextureBehaviour6.m
@@ -0,0 +1,36 @@
+// ======================================================================
+// Project Name    : ios_renderer_opengles1_client
+//
+// Copyright © 2020 U-CREATES. All rights reserved.
+//
+// This source code is the property of U-CREATES.
+// If such findings are accepted at any time.
+// We hope the tips and helpful in developing.
+// ======================================================================
+#import "TextureBehaviour6.h"
+@interface TextureBehaviour6 ()
+@end
+@implementation TextureBehaviour6
+@synthesize asset;
+- (id)init {
+    self = [super init];
+    NSBundle* bundle = [NSBundle mainBundle];
+    NSString* path = [bundle pathForResource:@"texture02" ofType:@"jpg"];
+    self->asset = [[CircleAsset1 alloc] init:0.5f divideCount:100 color:GLESColor.white];
+    [self->asset create:path];
+    return self;
+}
+- (void)onCreate:(Parameter*)parameter {
+    return;
+}
+- (void)onUpdate:(NSTimeInterval)delta {
+    [self->asset.transform setPosition:0.0f y:0.0f z:0.0f];
+    [self->asset.transform setScale:1.0f y:1.0f z:1.0f];
+    [self->asset.transform setRotation:0.0f y:0.0f z:0.0f];
+    return;
+}
+- (void)onDestroy {
+    [self->asset dispose];
+    return;
+}
+@end
