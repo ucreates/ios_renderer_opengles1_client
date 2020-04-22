71a0189024241839881bc94d55b26760a9bb0bcf
ucreates <ucreates.inc@gmail.com>
2020-04-22 09:20:34 +0900

    add shader demo

---
+++ objectivec/iOSObjectiveCClinet/Classes/Behaviour/ShaderBehaviour2.h
@@ -0,0 +1,22 @@
+// ======================================================================
+// Project Name    : ios_renderer_opengles1_client
+//
+// Copyright © 2020 U-CREATES. All rights reserved.
+//
+// This source code is the property of U-CREATES.
+// If such findings are accepted at any time.
+// We hope the tips and helpful in developing.
+// ======================================================================
+#ifndef SphereBehaviour2_h
+#define SphereBehaviour2_h
+#import "iOSObjectiveCFoundation.h"
+#import "iOSObjectiveCGLES1Renderer.h"
+@interface ShaderBehaviour2 : BaseBehaviour
+@property FiniteStateMachine<ShaderBehaviour2*>* stateMachine;
+@property BaseAsset* asset;
+- (id)init;
+- (void)onCreate:(Parameter*)parameter;
+- (void)onUpdate:(NSTimeInterval)delta;
+- (void)onDestroy;
+@end
+#endif /* TestBehaviour13_h */
---
+++ objectivec/iOSObjectiveCClinet/Classes/Behaviour/ShaderBehaviour2.m
@@ -0,0 +1,41 @@
+// ======================================================================
+// Project Name    : ios_renderer_opengles1_client
+//
+// Copyright © 2020 U-CREATES. All rights reserved.
+//
+// This source code is the property of U-CREATES.
+// If such findings are accepted at any time.
+// We hope the tips and helpful in developing.
+// ======================================================================
+#import "ShaderBehaviour2.h"
+@interface ShaderBehaviour2 ()
+@property double rotate;
+@end
+@implementation ShaderBehaviour2
+@synthesize asset;
+@synthesize rotate;
+- (id)init {
+    self = [super init];
+    GLES1ShaderAsset* shader = [[GLES1ShaderAsset alloc] init];
+    [shader setPhong];
+    self->asset = [[SphereAsset1 alloc] init:1.0f divideCount:30 color:GLESColor.red];
+    [self->asset setShader:shader];
+    [self->asset create];
+    return self;
+}
+- (void)onCreate:(Parameter*)parameter {
+    return;
+}
+- (void)onUpdate:(NSTimeInterval)delta {
+    [self->asset.transform setPosition:0.0f y:0.0f z:0.0f];
+    [self->asset.transform setScale:1.0f y:1.0f z:1.0f];
+    [self->asset.transform setRotation:self->rotate y:self->rotate z:self->rotate];
+    [self.timeLine next:delta];
+    self->rotate += 1.0f;
+    return;
+}
+- (void)onDestroy {
+    [self->asset dispose];
+    return;
+}
+@end
