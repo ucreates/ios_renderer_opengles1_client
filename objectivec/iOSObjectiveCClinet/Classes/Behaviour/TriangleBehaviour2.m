1cff7181ae87bac98e938d261bd45369956bb65d
ucreates <ucreates.inc@gmail.com>
2020-06-10 00:22:08 +0900

    add triangle2 demo

---
+++ objectivec/iOSObjectiveCClinet/Classes/Behaviour/TriangleBehaviour2.m
@@ -0,0 +1,35 @@
+// ======================================================================
+// Project Name    : ios_renderer_opengles1_client
+//
+// Copyright © 2020 U-CREATES. All rights reserved.
+//
+// This source code is the property of U-CREATES.
+// If such findings are accepted at any time.
+// We hope the tips and helpful in developing.
+// ======================================================================
+#import "TriangleBehaviour2.h"
+@interface TriangleBehaviour2 ()
+@end
+@implementation TriangleBehaviour2
+@synthesize asset;
+- (id)init {
+    self = [super init];
+    self->asset = [[TriangleAsset2 alloc] init:1.0f height:1.0f color:GLESColor.white];
+    [self->asset create];
+    return self;
+}
+- (void)onCreate:(Parameter*)parameter {
+    return;
+}
+- (void)onUpdate:(NSTimeInterval)delta {
+    [self->asset.transform setPosition:0.0f y:0.0f z:0.0f];
+    [self->asset.transform setScale:1.0f y:1.0f z:1.0f];
+    [self->asset.transform setRotation:0.0f y:0.0f z:0.0f];
+    [self->asset.vertex setRandomColor];
+    return;
+}
+- (void)onDestroy {
+    [self->asset dispose];
+    return;
+}
+@end
