87c6d450e7bee62ad7d55dfdf051f1b20161a115
ucreates <ucreates.inc@gmail.com>
2020-10-28 09:28:49 +0900

    add sphere3 texture demo

---
+++ objectivec/iOSObjectiveCClinet/Classes/ViewController/TextureViewController22.m
@@ -0,0 +1,73 @@
+// ======================================================================
+// Project Name    : ios_renderer_opengles1_client
+//
+// Copyright © 2019 U-CREATES. All rights reserved.
+//
+// This source code is the property of U-CREATES.
+// If such findings are accepted at any time.
+// We hope the tips and helpful in developing.
+// ======================================================================
+#import "TextureViewController22.h"
+#import <GLKit/GLKit.h>
+#import <OpenGLES/ES1/gl.h>
+#import <OpenGLES/ES1/glext.h>
+#import "TextureBehaviour22.h"
+#import "iOSObjectiveCFoundation.h"
+#import "iOSObjectiveCGLES1Renderer.h"
+@interface TextureViewController22 ()
+@property GLES1Renderer* renderer;
+@property NSMutableArray<TextureBehaviour22*>* behaviours;
+@end
+@implementation TextureViewController22
+@synthesize renderer;
+@synthesize behaviours;
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.delegate = self;
+    self.preferredFramesPerSecond = 60;
+    self->renderer = [[GLES1Renderer alloc] init];
+    [self->renderer create];
+    [self->renderer.camera setFov:60.0f];
+    [self->renderer.camera setClippingPlane:0.1f farPlane:100.0f];
+    [self->renderer.camera setLookAt:GLKVector3Make(0.0f, 0.0f, -5.0f) center:GLKVector3Make(0.0f, 0.0f, 0.0f) up:GLKVector3Make(0.0f, 1.0f, 0.0f)];
+    self->behaviours = [[NSMutableArray<TextureBehaviour22*> alloc] init];
+    for (int i = 0; i < 1; i++) {
+        TextureBehaviour22* behaviour = [[TextureBehaviour22 alloc] init];
+        [self->behaviours addObject:behaviour];
+    }
+    GLKView* glkView = (GLKView*)self.view;
+    glkView.context = self->renderer.context;
+    glkView.drawableDepthFormat = GLKViewDrawableDepthFormat16;
+    CAEAGLLayer* caglLayer = (CAEAGLLayer*)self.view.layer;
+    caglLayer.drawableProperties = @{kEAGLDrawablePropertyRetainedBacking : @NO, kEAGLDrawablePropertyColorFormat : kEAGLColorFormatRGBA8};
+    [caglLayer setOpaque:YES];
+    CGSize size = UIScreen.mainScreen.nativeBounds.size;
+    [self->renderer bind:caglLayer width:size.width height:size.height];
+    return;
+}
+- (void)viewWillLayoutSubviews {
+    CAEAGLLayer* caglLayer = (CAEAGLLayer*)self.view.layer;
+    [self->renderer rebind:caglLayer];
+    return;
+}
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+    for (TextureBehaviour22* behaviour in self->behaviours) {
+        [behaviour onDestroy];
+    }
+    [self->renderer delete];
+    return;
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+- (void)glkViewControllerUpdate:(nonnull GLKViewController*)controller {
+    [renderer transform:kDimension3D];
+    for (TextureBehaviour22* behaviour in self->behaviours) {
+        [behaviour onUpdate:self.timeSinceLastUpdate];
+        [renderer render:behaviour.asset];
+    }
+    [renderer present];
+    return;
+}
+@end
